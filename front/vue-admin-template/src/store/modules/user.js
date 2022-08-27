fb74e21bb6cc1eef44d57a39670389c08d3cbdbe
SansZhu <707643309@qq.com>
2022-08-28 01:41:01 +0800

    test1

--- front/vue-admin-template/src/store/modules/user.js
+++ front/vue-admin-template/src/store/modules/user.js
@@ -28,7 +28,7 @@ const mutations = {
 }
 
 const actions = {
-  // user login 1111
+  // user login 111111
   login({ commit }, userInfo) {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
